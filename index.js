d25738679905030b4fda75d82bd00b484bd5a745
Wadad Parker <wadadparker5@gmail.com>
2024-12-20 05:18:42 +0530

    feat: integrate prisma client

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
-const app = express();
+import prisma from './prismaClient.js'
 
+const app = express();
 const port = 3000
 
 app.use(express.json());
@@ -8,6 +9,21 @@ app.use(express.json());
 app.get('/',(req,res)=>{
     res.send("welcome to AnimeCon Event Management App");
 });
+
+app.post('/users',async (req,res)=>{
+    const name = "Wadad parker"
+    const email = "wadadparker5@gmail.com"
+    const googleId= "123"
+    try {
+        const user = await prisma.user.create({
+            data: {name,email,googleId},
+        })
+        res.json(user);
+    }
+    catch(error) {
+        res.status(500).json({error:error.message})
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
