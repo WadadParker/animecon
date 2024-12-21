fc972cfaf588f6fa3f7b0866a26bd51788349695
Wadad Parker <wadadparker5@gmail.com>
2024-12-21 11:35:22 +0530

    feat: poll routes

---
+++ routes/pollRoutes.js
@@ -0,0 +1,17 @@
+// routes/pollRoutes.js
+
+import { Router } from 'express';
+const pollRouter = Router();
+import { answerPoll, checkPollAnswered } from '../controllers/pollController';
+import { verifyToken } from '../middleware/jwtMiddleware';
+
+// Apply JWT middleware
+pollRouter.use(verifyToken);
+
+// 8. Answer a Poll
+pollRouter.post('/:pollId/answer', answerPoll);
+
+// 9. Check if Poll is answered by User
+pollRouter.get('/:pollId/answer-status', checkPollAnswered);
+
+export default pollRouter;
