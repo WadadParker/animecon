64c6fd7f819eec1e9f1a7ecd23bfa20c1f3753cc
Wadad Parker <wadadparker5@gmail.com>
2024-12-21 11:38:13 +0530

    feat: user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,23 @@
+// routes/userRoutes.js
+
+import { Router } from 'express';
+const userRouter = Router();
+import { checkPaymentStatus, updatePaymentStatus, getUserDetails, scratchAnimeCard } from '../controllers/userController';
+import { verifyToken } from '../middleware/jwtMiddleware';
+
+// Apply JWT middleware to all routes in this router
+userRouter.use(verifyToken);
+
+// 2. Check whether user has paid or not
+userRouter.get('/payment-status', checkPaymentStatus);
+
+// 3. Update user value to paid
+userRouter.patch('/payment-status', updatePaymentStatus);
+
+// 4. Get user's details
+userRouter.get('/me', getUserDetails);
+
+// 5. Scratch the anime card
+userRouter.post('/scratch-anime', scratchAnimeCard);
+
+export default userRouter;
