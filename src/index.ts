b39f6824bc78fe5ded99924dbe1d29109929234b
Wadad Parker <wadadparker5@gmail.com>
2025-03-03 07:10:40 +0530

    refactor: setup routes according to elysia best practices

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,11 @@ import { userRoutes } from "./routes/users.routes";
 
 const app = new Elysia()
 
-app.get("/", () => "Hello Elysia")
-app.post("/users/signup",()=>{console.log("This is signup"); return {message:"Works",status:200}});
+.get("/", () => "Hello Elysia")
 
-app.listen(3000);
+.use(userRoutes)
+.listen(3000);
 
-userRoutes(app)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,8 @@
 import Elysia from "elysia";
 
-export const userRoutes = (app: Elysia) => {
-
-    app.post("/users/signup",()=>{console.log("This is signup"); return {message:"Works",status:200}});
-    app.post("/users/login",()=>console.log("This is login"));
-}
+export const userRoutes = new Elysia()
+    .group("/users",(app)=>
+        app
+            .post("/signup",()=>{console.log("This is signup"); return {message:"Works",status:200}})
+            .post("/login",()=>console.log("This is login"))
+);
