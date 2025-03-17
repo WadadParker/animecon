3c24fd6406e4f1ebef2943a0be1092558403de17
Wadad Parker <wadadparker5@gmail.com>
2025-03-17 18:15:07 +0530

    feat: login API

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -18,6 +18,8 @@ new Elysia( {prefix:"users"} )
             password: t.String(),
         })
 
+        console.log("API request for Signup")
+
         const { name, email, password } = body as {name:string,email:string,password:string}
 
         set.headers["access-control-allow-methods"] = "POST","GET"
@@ -33,7 +35,8 @@ new Elysia( {prefix:"users"} )
                 set.status = 200
         
                 return { message: "User created Succesfully!" , jwt:result[0]}
-        } catch (error: any) {
+        } 
+        catch (error: any) {
             // PostgreSQL unique constraint error code is typically "23505"
             if(error?.errno == 23505){
             console.log("Error happened",error)
@@ -46,4 +49,43 @@ new Elysia( {prefix:"users"} )
     
 
     })
-    .post("/login",()=>console.log("This is login"))
+    .post("/login", async ({set,body})=>{
+        body: t.Object({
+            email: t.String(),
+            password: t.String(),
+        })
+        
+        console.log("API request for Login")
+        const { email, password } = body as {email:string,password:string};
+
+        set.headers["access-control-allow-methods"] = "POST","GET"
+        set.headers["access-control-allow-origin"] = "*"
+        set.headers["access-control-allow-headers"] = 'Content-Type, Origin, Accept, Authorization ';
+
+        try {
+            const result = await sql`
+                SELECT user_id,paid
+                FROM users 
+                WHERE
+                    email = ${email}
+                    AND password = ${password}
+                `
+
+                console.log("This is result",result[0])
+                if(result[0])
+                {
+                    set.status = 200
+                    return { message: "User Logged In" , jwt:result[0] , paid:result[0]?.paid}
+                }
+
+                set.status = 400
+                return { message: "Incorrect email/password"}
+
+        } 
+        catch (error: any) {
+            set.status = 422;
+            console.log("Error in DB opertion",error)
+            return { error: "DB Operation Failed" }
+          }
+
+    })
